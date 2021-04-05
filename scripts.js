f27b6de5621003cc8ed01709ae821a9564fec857
A M <amymilton99999@gmail.com>
2021-04-05 18:34:57 -0500

    modified all but directional buttons

--- scripts.js
+++ scripts.js
@@ -1,2 +1,33 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+function init (){
+    let flight = document.getElementById("flightStatus");
+    let takeOff = document.getElementById("takeoff");
+    let bkgrnd = document.getElementById("shuttleBackground");
+    let shuttleHeight = document.getElementById("spaceShuttleHeight");
+    let land = document.getElementById("landing");
+    let abort = document.getElementById("missionAbort");
+    let direction = getElementsByTagName("button");
+        takeOff.addEventListener("click", function(){
+            let ready = window.confirm("Confirm that shuttle is ready for takeoff.");
+            if (ready === true) {
+                flight.innerHTML = "Shuttle in flight."
+                bkgrnd.style.background = 'blue';
+                shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML) + 10000;
+            }
+        })
+        land.addEventListener("click", function(){
+           window.alert("The shuttle is landing. Landing gear engaged.");
+           flight.innerHTML = "The shuttle has landed."
+           bkgrnd.style.background = 'green';
+           shuttleHeight.innerHTML = 0;
+        })
+        abort.addEventListener("click", function(){
+            let confirmAbort = window.confirm("Confirm that you want to abort the mission");
+            if (confirmAbort === true){
+                flight.innerHTML = "Mission aborted."
+                bkgrnd.style.background = 'green';
+                shuttleHeight.innerHTML = 0;
+            }
+        })
+}
+
+window.addEventListener("load", init);
