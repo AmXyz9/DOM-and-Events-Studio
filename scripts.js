e94af49d366a45e978b9ca062d82d83286886380
A M <amymilton99999@gmail.com>
2021-04-06 20:36:53 -0500

    up down left right

--- scripts.js
+++ scripts.js
@@ -5,7 +5,13 @@ function init (){
     let shuttleHeight = document.getElementById("spaceShuttleHeight");
     let land = document.getElementById("landing");
     let abort = document.getElementById("missionAbort");
-    let direction = getElementsByTagName("button");
+    let up = document.getElementById("up");
+    let down = document.getElementById("down");
+    let right = document.getElementById("right");
+    let left = document.getElementById("left");
+    let rocket = document.getElementById('rocket');
+    let shuttleHoriz = 0;
+    let shuttleVert = 0;
         takeOff.addEventListener("click", function(){
             let ready = window.confirm("Confirm that shuttle is ready for takeoff.");
             if (ready === true) {
@@ -27,6 +33,26 @@ function init (){
                 bkgrnd.style.background = 'green';
                 shuttleHeight.innerHTML = 0;
             }
+        })
+        up.addEventListener("click", function(){
+            shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML) + 10000;
+            shuttleVert -= 10;
+            rocket.style.top = shuttleVert + 'px';
+        })
+        down.addEventListener("click", function(){
+            if (Number(shuttleHeight.innerHTML) >= 10000) {
+            shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML) - 10000;
+            }
+            shuttleVert += 10;
+            rocket.style.top = shuttleVert + 'px';
+        })
+        right.addEventListener("click", function(){
+            shuttleHoriz += 10;
+            rocket.style.left = shuttleHoriz + 'px';
+        })
+        left.addEventListener("click", function(){
+            shuttleHoriz -= 10;
+            rocket.style.left = shuttleHoriz + 'px';
         })
 }
 
